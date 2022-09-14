dc5ac96b3d704a72424381d68758fa5c78fb05b7
unknown <zaag.malek1@gmail.com>
2022-09-14 09:33:51 +0100

    adding tailwind config

--- client/pages/signup.jsx
+++ client/pages/signup.jsx
@@ -1,6 +1,19 @@
 export default function Signup() {
   return (
     <>
+      <h1 className="text-3xl font-bold underline">hello</h1>
+      <div>
+        <form className="text-3xl font-bold underline">
+          <label for="firstname"></label>
+          <input name="firstname"></input>
+          <label for="lastname"></label>
+          <input name="lastname"></input>
+          <label for="email"></label>
+          <input name="email"></input>
+          <label for="password"></label>
+          <input name="password"></input>
+        </form>
+      </div>
       <footer>footer test</footer>
     </>
   );
