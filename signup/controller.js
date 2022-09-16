3e6afc61770953b40b4b4d7caf97658b6bc4540b
unknown <zaag.malek1@gmail.com>
2022-09-16 14:37:13 +0100

    adding dockerfile and running the image

--- signup/controller.js
+++ signup/controller.js
@@ -3,15 +3,24 @@ const { User } = require("./model");
 module.exports.createUser = (req, res) => {
   const { firstname, lastname, email, password } = req.body;
   console.log(firstname, lastname, email, password);
-  User.findOne({ email: email })
-    .then((response) => console.log(response))
+  const user = User.findOne({ email: email })
+    .then((response) => {
+      console.log(response);
+      res.status(400);
+      res.send("User found in database");
+    })
     .catch((err) => console.log(err));
-  User.create({
-    firstname,
-    lastname,
-    email,
-    password,
-  })
-    .then((response) => res.send("user added successfully to database"))
-    .catch((err) => console.log(err));
+  if (!user) {
+    User.create({
+      firstname,
+      lastname,
+      email,
+      password,
+    })
+      .then((response) => {
+        res.send("User added successfully to database");
+        res.status(200);
+      })
+      .catch((err) => console.log(err));
+  }
 };
